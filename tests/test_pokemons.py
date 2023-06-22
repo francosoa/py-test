fe2f9765ca73724aeb67409c2079c032bb02bf7f
francosoa <francosoamota@gmail.com>
2023-06-22 20:20:15 -0300

    add teste number one

---
+++ tests/test_pokemons.py
@@ -0,0 +1,16 @@
+from codigo.poke import Pokemons
+
+class TestClass:
+
+    def test_verify_the_type_of_result(self):
+        #Given > quando passa uma URI
+        input = 'https://pokemonapi.franciscovaldec.repl.co/pokemon'
+        output = type(list())
+
+        request = Pokemons(input)
+        result = request.GetAllPokemon()
+
+
+        assert output == type(result)
+
+        pass
