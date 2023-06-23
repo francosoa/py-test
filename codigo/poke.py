58325de5173edb2fa61c5bb5c809790c5911a23e
francosoa <francosoamota@gmail.com>
2023-06-23 19:20:08 -0300

    check my dataframe

--- codigo/poke.py
+++ codigo/poke.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import json
 
 class Pokemons:
     def __init__(self, url):
@@ -16,3 +17,16 @@ class Pokemons:
         return result
 
 
+    def GetDataFrame(self):
+        response = requests.get(self.url)
+        json_result = response.json()
+
+        df = pd.json_normalize(json_result)
+
+        return df
+
+
+all_api = Pokemons('https://pokemonapi.franciscovaldec.repl.co/pokemon')
+
+df = all_api.GetDataFrame()
+print(df)
--- tests/test_pokemons.py
+++ tests/test_pokemons.py
@@ -1,4 +1,5 @@
 from codigo.poke import Pokemons
+import pandas as pd
 
 class TestClass:
 
@@ -12,3 +13,16 @@ class TestClass:
 
 
         assert output == type(result)
+
+    def test_check_with_my_function_is_a_datafrane(self):
+        #given = We want transform my json result in a dataframe to analysis:
+        #action: When the path of API is given
+        call_api = Pokemons('https://pokemonapi.franciscovaldec.repl.co/pokemon')
+
+        #resull must be a dataframe:
+        result = "<class 'pandas.core.frame.DataFrame'>"
+
+        df = call_api.GetDataFrame()
+
+        assert type(df) == type(pd.DataFrame())
+
