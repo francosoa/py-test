8b2606ec1dcd05ce40ea8867c5f5567821091a8c
francosoa <francosoamota@gmail.com>
2023-06-27 20:51:42 -0300

    remove duplicate of code

--- tests/test_pokemons.py
+++ tests/test_pokemons.py
@@ -1,39 +1,45 @@
+import pytest
 from codigo.poke import Pokemons
 import pandas as pd
 from pytest import mark as marker
 
 
 class TestClass:
-    @marker.dataframe_result
-    def test_verify_the_type_of_result(self):
-        # Given > when give an URI
+
+    @pytest.fixture
+    def test_get_result_from_my_api(self):
+
         input = 'https://pokemonapi.franciscovaldec.repl.co/pokemon'
+        request = Pokemons(input)
+
+        return request
+
+    @marker.dataframe_result
+    def test_verify_the_type_of_result(self, test_get_result_from_my_api):
+        response = test_get_result_from_my_api.GetAllPokemon()
         output = type(list())
 
-        request = Pokemons(input)
-        result = request.GetAllPokemon()
 
-        assert output == type(result)
+        assert output == type(response)
 
     @marker.dataframe_result
-    def test_check_with_my_function_is_a_datafrane(self):
+    def test_check_with_my_function_is_a_datafrane(self, test_get_result_from_my_api):
         # given = We want transform my json result in a dataframe to analysis:
         # action: When the path of API is given
-        call_api = Pokemons('https://pokemonapi.franciscovaldec.repl.co/pokemon')
+
         # resull must be a dataframe
 
-        df = call_api.GetDataFrame()
+        df = test_get_result_from_my_api.GetDataFrame()
 
         assert type(df) == type(pd.DataFrame())
 
-    def test_check_if_there_is_null_in_my_dataframe(self):
+    def test_check_if_there_is_null_in_my_dataframe(self, test_get_result_from_my_api):
         # given = the dataset can be with NAN values
         # action = When someone given the URI of API
-        response = Pokemons('https://pokemonapi.franciscovaldec.repl.co/pokemon')
         # the result must be zero
         result = 0
 
-        df = response.GetDataFrame()
+        df = test_get_result_from_my_api.GetDataFrame()
         check_null = list(df.isna().sum())
 
         assert sum(check_null) == result
